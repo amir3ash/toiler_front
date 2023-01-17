f5c074e1aa7f455f04ea1ca11322ba23e4265f91
Amir Shams <amir3.ash@gmail.com>
2023-01-17 19:11:37 +0330

    patch bg-color of assignees.

--- src/components/Cards/AssignedSelectionItem.svelte
+++ src/components/Cards/AssignedSelectionItem.svelte
@@ -17,9 +17,9 @@
 
 
 {#each value as item, i}
-     <div class="flex my-1 px-2 py-1  items-center text-ellipsis overflow-hidden whitespace-nowrap rounded-full bg-zinc-100 ">
-        <span class="w-6 h-6 bg-blueGray-200 inline-flex items-center justify-center rounded-full">
-            <img class="w-full rounded-full align-middle border-none shadow-lg" src="{item.avatar}">
+     <div class="flex my-1 px-2 py-1  items-center text-ellipsis overflow-hidden whitespace-nowrap rounded-full bg-zinc-100 dark:bg-zinc-700">
+        <span aria-hidden="true" class="w-6 h-6 bg-blueGray-200 dark:bg-blueGray-800 inline-flex items-center justify-center rounded-full">
+            <img class="w-full rounded-full align-middle border-none shadow-lg" src="{item.avatar}" alt="">
         </span>
         <div class="overflow-hidden text-ellipsis whitespace-nowrap">
             <div class="inline-block ml-1">{item.username}</div>
--- src/components/Cards/UserIcon.svelte
+++ src/components/Cards/UserIcon.svelte
@@ -4,7 +4,7 @@
 </script>
 
 {#if show}
-    <span class="w-6 h-6 bg-blueGray-200 inline-flex items-center justify-center rounded-full" aria-hidden="true">
+    <span class="w-6 h-6 bg-blueGray-200 dark:bg-blueGray-700 inline-flex items-center justify-center rounded-full" aria-hidden="true">
         <img class="w-full rounded-full align-middle border-none shadow-lg" src="{avatar}">
     </span>
 {/if}
