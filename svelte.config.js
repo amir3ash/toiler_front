3c13eb503650608a4fa011dd8c02ccaeb5523222
Amir Shams <amir3.ash@gmail.com>
2023-03-13 21:32:42 +0330

    installing `svelte-reactive-preprocessor`

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,17 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { reactivePreprocess } from 'svelte-reactive-preprocessor'
+
+const debuging = false
+const preprocess = [vitePreprocess()]
+
+if (debuging)
+  preprocess.push(reactivePreprocess({
+    enabled: true,
+    state: true // for debuging set theme true
+  }))
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: preprocess
 }
